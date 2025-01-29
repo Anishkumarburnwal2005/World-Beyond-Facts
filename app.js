a53cb085c9e653ca04acc6423c54fcee6a89a552
Anishkumarburnwal2005 <anishkumarburnwal9@gmail.com>
2025-01-29 11:16:01 +0530

    Router work completed

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const path = require("path");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended:true}));
+const methodOverride = require("method-override");
+app.use(methodOverride("_method"));
 
 main()
 .then(() => {
@@ -46,6 +48,11 @@ app.post("/facts/create",async (req, res) => {
     const babu = await fact1.save();
     console.log(babu);
     res.redirect("/facts");
+
+    // const newFact = await new Facts(req.body.fact);
+    // await newFact.save();
+    // console.log(newFact);
+    // res.redirect("/facts");
 });
 
 //Show route
@@ -53,6 +60,36 @@ app.get("/facts/:id",async (req, res) => {
     const {id} = req.params;
     const fact = await Facts.findById(id);
     res.render("facts/show.ejs", {fact}); 
+});
+
+//Edit route
+app.get("/facts/edit/:id",async (req, res) => {
+    const {id} = req.params;
+    const fact = await Facts.findById(id);
+    res.render("facts/edit.ejs", {fact});
+});
+
+//Update route
+app.put("/facts/:id",async (req, res) => {
+    const {id} = req.params;
+    const {fact, category, source, data, verified, addedBy, updated} = req.body;
+    await Facts.findByIdAndUpdate(id, {
+        fact: fact,
+        category: category,
+        source: source,
+        data_discovered: data,
+        is_verified: verified,
+        added_by: addedBy,
+        updated_at: updated
+    });
+    res.redirect("/facts");
+});
+
+//Delete route
+app.delete("/facts/delete/:id",async (req, res) => {
+    const {id} = req.params;
+    await Facts.findByIdAndDelete(id);
+    res.redirect("/facts");
 });
 
 //root route
